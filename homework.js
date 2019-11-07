c09eb113556e95db1e8fa340745f1f0882dc3327
Jae-Hwang <jae.hwang@revature.com>
2019-11-06 20:47:54 -0500

    finished JS Q8

--- homework.js
+++ homework.js
@@ -18,7 +18,7 @@ function fib(n) {
     let prev = 1;
     let prevprev = 1;
     let cur = 1;
-    for(let i = 2; i < n; i++) {
+    for (let i = 2; i < n; i++) {
         cur = prev + prevprev;
         prevprev = prev;
         prev = cur;
@@ -37,12 +37,12 @@ function bubbleSort(inputArray) {
         let limit;
         let temp;
 
-        for (let i = 0; i < numArray.length-1; i++) {
-            limit = numArray.length-i;
-            for (let ii = 0; ii < limit-1; ii++) {
-                if (numArray[ii] > numArray[ii+1]) {
-                    temp = numArray[ii+1];
-                    numArray[ii+1] = numArray[ii];
+        for (let i = 0; i < numArray.length - 1; i++) {
+            limit = numArray.length - i;
+            for (let ii = 0; ii < limit - 1; ii++) {
+                if (numArray[ii] > numArray[ii + 1]) {
+                    temp = numArray[ii + 1];
+                    numArray[ii + 1] = numArray[ii];
                     numArray[ii] = temp;
                 }
             }
@@ -60,7 +60,7 @@ function reverseStr(someStr) {
     strArray = Array.from(someStr);
     result = '';
     for (let i = 0; i < strArray.length; i++) {
-        result += strArray[strArray.length-1-i];
+        result += strArray[strArray.length - 1 - i];
     }
     return result;
 }
@@ -73,7 +73,7 @@ function reverseStr(someStr) {
 function factorial(n) {
     if (typeof n === 'number') {
         //console.log('is number');
-        return (n>0)?n*fib(n-1):1;
+        return (n > 0) ? n * fib(n - 1) : 1;
     }
     console.log("input is not number.")
 }
@@ -87,15 +87,15 @@ function substring(someStr, length, offset) {
     if (length === 0) {
         return '';
     }
-    if (offset < 0 || offset > someStr.length || offset+length > someStr.length) {
+    if (offset < 0 || offset > someStr.length || offset + length > someStr.length) {
         alert(`Index out of bound: cannot access characters outside of input string.`);
         return;
     }
     let strArray = Array.from(someStr);
     let result = '';
-    for(let i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         //console.log(strArray[i+offset]);
-        result += strArray[i+offset];
+        result += strArray[i + offset];
     }
     return result;
 }
@@ -106,10 +106,10 @@ function substring(someStr, length, offset) {
 // Return true if even, false if odd.
 // Do not use % operator.
 function isEven(someNum) {
-    if (typeof n !== 'number') {
+    if (typeof someNum !== 'number') {
         return console.log("input is not number.");
     }
-    return (someNum/2).toString().includes('.');
+    return !(someNum / 2).toString().includes('.');
 }
 
 
@@ -142,11 +142,88 @@ function isPalindrome(someStr) {
 // *****
 //  ***
 //   *
+function printShape(shape, height, character) {
+    // -- checks for invalid inputs -- //
+    if (typeof height !== 'number') {
+        return console.log("input height is not number.");
+    }
+    if (isEven(height)) {
+        return console.log("please, input odd number for the height.")
+    }
+    if (character.length !== 1) {
+        return console.log("pleasem input 1 character for character input.")
+    }
 
+    // -- checks for shape keyword -- //
+    if (shape === 'Square') {
+        pSquare(height, character);
+    }
+
+    if (shape === "Triangle") {
+        pTri(height, character);
+    }
+
+    if (shape === "Diamond") {
+        pDia(height, character);
+    }
+
+    // -- input shape does not match keywords -- //
+    return console.log("Input shape does not match.")
+}
+
+// for "Square"
+function pSquare(height, character) {
+    let resultStr = '';
+    for (let i = 0; i < height; i++) {
+        for (let ii = 0; ii < height; ii++) {
+            resultStr += character;
+        }
+        resultStr += '\n';
+    }
+    console.log(resultStr);
+}
+// for "Triangle"
+function pTri(height, character) {
+    let resultStr = '';
+    for (let i = 0; i < height; i++) {
+        for (let ii = 0; ii < i + 1; ii++) {
+            resultStr += character;
+        }
+        resultStr += '\n';
+    }
+    console.log(resultStr);
+}
+// for "Diamond"
+function pDia(height, character) {
+    let resultStr = '';
+    for (let i = 0; i < height; i++) {
+        resultStr += diaHelper(i + 1, height, character);
+        resultStr += '\n';
+    }
+    console.log(resultStr);
+}
+// prints a row for diamond
+function diaHelper(row, width, character) {
+    if (row > Math.ceil(width / 2)) {
+        row = width - row + 1;
+    }
+
+    result = ''
+    for (let i = 0; i < row * 2 - 1; i++) {
+        result += character;
+    }
+    while (result.length < width) {
+        result = ' ' + result + ' ';
+    }
+    return result;
+}
 
 // 9. Object literal
 // Define function traverseObject(someObj)
 // Print every property and it's value.
+function traverseObject(someObj) {
+
+}
 
 
 // 10. Delete Element
@@ -155,7 +232,6 @@ function isPalindrome(someStr) {
 // Delete the third element in the array.
 // Print length
 // The lengths should be the same.
-
 
 // 11. Splice Element
 // Define function spliceElement(someArr)
